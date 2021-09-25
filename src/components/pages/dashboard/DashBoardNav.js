415384219241d3947a2016035b9d32bb963f14e5
Priyank <priyankcrjr7@gmail.com>
2021-09-25 20:19:43 +0530

    added react router navlinks

--- src/components/pages/dashboard/DashBoardNav.js
+++ src/components/pages/dashboard/DashBoardNav.js
@@ -1,6 +1,7 @@
 import classes from "./DashBoardNav.module.css";
 import AIT from "../../../assets/logos/AIT black logo.png";
 import jobImg from "../../../assets/images/jobprofiles.svg";
+import { NavLink } from "react-router-dom";
 const DashBoardNav = () => {
   return (
     <div className={classes.dashBoardNav}>
@@ -10,18 +11,41 @@ const DashBoardNav = () => {
           <h1>Placement cell</h1>
         </div>
         <div className={classes["dashboard-nav-links"]}>
-          <div className={classes["nav-link"]}>
-            <a href="">DashBoard</a>
-          </div>
-          <div className={classes["nav-link"]}>
-            <a href="">Account</a>
-          </div>
-          <div className={classes["nav-link"]}>
-            <a href="">Analytics</a>
-          </div>
-          <div className={classes["nav-link"]}>
-            <a href="">Companies</a>
-          </div>
+          <NavLink
+            to="/dashboard/posts"
+            activeClassName={classes["active-link"]}
+          >
+            <div className={classes["nav-link"]}>
+              <a href="">DashBoard</a>
+            </div>
+          </NavLink>
+
+          <NavLink
+            to="/dashboard/account"
+            activeClassName={classes["active-link"]}
+          >
+            <div className={classes["nav-link"]}>
+              <a href="">Account</a>
+            </div>
+          </NavLink>
+
+          <NavLink
+            to="/dashboard/analytics"
+            activeClassName={classes["active-link"]}
+          >
+            <div className={classes["nav-link"]}>
+              <a href="">Analytics</a>
+            </div>
+          </NavLink>
+
+          <NavLink
+            to="/dashboard/companies"
+            activeClassName={classes["active-link"]}
+          >
+            <div className={classes["nav-link"]}>
+              <a href="">Companies</a>
+            </div>
+          </NavLink>
         </div>
       </div>
       <div className={classes["nav-bottom"]}>
