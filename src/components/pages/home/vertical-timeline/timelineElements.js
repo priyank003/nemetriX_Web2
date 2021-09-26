d84d92e4a9cab54b8a2548361596c2394c37e4e1
Priyank <priyankcrjr7@gmail.com>
2021-09-27 00:02:49 +0530

    added vertical timeline

---
+++ src/components/pages/home/vertical-timeline/timelineElements.js
@@ -0,0 +1,47 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "32 LPA highest pakage at amazon",
+    percentage: "96",
+    description: "Over 50 companies vosited for placement drive",
+    year: "2016-17",
+    icon: "amazon",
+  },
+  {
+    id: 2,
+    title: "39 LPA highest pakage at microsoft",
+    percentage: "93",
+    description: "Over 70 companies vosited for placement drive",
+    year: "2017-18",
+    icon: "microsoft",
+  },
+  {
+    id: 3,
+    title: " 39 LPA highest package offred by google",
+    percentage: "95",
+    description:
+      "Over 80 copanies visted campus offering most of ttye packages above 10LPA ",
+    year: "2018-19",
+    icon: "google",
+  },
+  {
+    id: 4,
+    title: "39 LPA offered by amazon to 5 students ",
+    percentage: "96",
+    description:
+      "100 companies visited placement drive making our college hotspot for placements in pune",
+    year: "2019-20",
+    icon: "amazon",
+  },
+  {
+    id: 5,
+    title: "39 LPA offred by google to 10 of our students",
+    percentage: "98",
+    description:
+      "spite of pandemic our institute saw rise in no of companies visiting for placement drives",
+    year: "2020-21",
+    icon: "google",
+  },
+];
+
+export default timelineElements;
