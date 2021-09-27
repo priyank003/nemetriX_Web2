35f8d94f61d7056719370384136bd6784402b057
Priyank <priyankcrjr7@gmail.com>
2021-09-27 15:37:31 +0530

    created redux store

---
+++ src/components/store/index.js
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
+export const authActions = authSlice.actions;
+export default store;
