9e0ea936e2eafcf2e788a12e87c5cfe05986f323
Priyank <priyankcrjr7@gmail.com>
2021-09-27 15:36:50 +0530

    added company post

---
+++ src/components/pages/dashboard/CompanyPost.js
@@ -0,0 +1,24 @@
+import classes from "./CompanyPost.module.css";
+import google from "../../../assets/logos/Google__G__Logo.svg.png";
+const CompanyPost = () => {
+  return (
+    <div className={classes.companypost}>
+      <div className={classes["company-post-container"]}>
+        <div className={classes["company-post-header"]}>
+          <div className={classes["company-logo"]}>
+            <img src={google}></img>
+          </div>
+          <div className={classes["company-name"]}>
+            <h3>Google</h3>
+          </div>
+        </div>
+
+        <div className={classes["company-category"]}>Software</div>
+        {/* <div className={classes["company-job-profile"]}>SWE</div> */}
+        <div className={classes["company-job-vacancy"]}>25</div>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyPost;
