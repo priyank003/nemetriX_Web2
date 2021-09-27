c491c1b7121345db684d7909061f46e69e102ab2
Priyank <priyankcrjr7@gmail.com>
2021-09-28 01:16:20 +0530

    responsive

--- src/components/pages/dashboard/DashBoard.js
+++ src/components/pages/dashboard/DashBoard.js
@@ -1,55 +1,21 @@
 import classes from "./DashBoard.module.css";
 import DashBoardNav from "./DashBoardNav";
-import search from "../../../assets/logos/search_black_24dp.svg";
-import calendarIcon from "../../../assets/logos/today_black_24dp.svg";
-import bellIcon from "../../../assets/logos/bell.png";
-import userImg from "../../../assets/images/user/cheerful-curly-business-girl-wearing-glasses.jpg";
+
 import DashBoardCalendar from "./DashBoardCalendar";
 import rocketicon from "../../../assets/logos/rocket.svg";
 import interview from "../../../assets/logos/interview.svg";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import DashBoardPages from "./DashBoardPages";
-import { useSelector } from "react-redux";
 
+import DashboardHeader from "./DashboardHeader";
 const DashBoard = () => {
-  const isAuth = useSelector((state) => state.auth.isAuthenticated);
-
-  console.log(isAuth);
-
   return (
     <div className={classes.dashboard}>
       <div className={classes["dashboard-container"]}>
         <DashBoardNav />
+
         <div className={classes["dashboard-main"]}>
-          <div className={classes["dashboard-main-header"]}>
-            <div className={classes["dashboard-search"]}>
-              <span>Dashboard</span>
-
-              <div className={classes["search-input"]}>
-                <input type="text" placeholder="Search Jobs" />
-                <div className={classes["search-icon"]}>
-                  <img src={search} alt="" />
-                </div>
-              </div>
-            </div>
-            <div className={classes["dashboard-user"]}>
-              <div className={classes["user-date"]}>
-                {" "}
-                <img src={calendarIcon} alt="" />
-                <span> 24 sept, 2021</span>
-              </div>
-
-              <div className={classes["user-notifications"]}>
-                <img src={bellIcon} alt="" />
-                <span> 3 </span>
-              </div>
-              <div className={classes["user-profile"]}>
-                <Link to="/dashboard/account">
-                  <img src={userImg} alt="" />
-                </Link>
-              </div>
-            </div>
-          </div>
+          <DashboardHeader />
           <div className={classes["dashboard-main-container"]}>
             <div className={classes["main-left"]}>
               <DashBoardPages />
