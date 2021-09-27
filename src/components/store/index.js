0d17550b32a6c4c7701bb12ff6b21aa9eadf11a8
Priyank <priyankcrjr7@gmail.com>
2021-09-28 01:30:07 +0530

    updated redux store

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,26 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import authSlice from "./auth-slice";
+import sideNavSlice from "./sideNav-slice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    sideNav: sideNavSlice.reducer,
   },
 });
-export const authActions = authSlice.actions;
+
 export default store;
